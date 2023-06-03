5053227154ff58e03acfee5646f0fd087fd83dd5
Shadowstrikesblu <hmoulk8@gmail.com>
2023-06-03 21:30:16 +0200

    fixes

---
+++ src/Part1/Print.h
@@ -0,0 +1,14 @@
+//
+// Created by hmoul on 20/05/2023.
+//
+
+#ifndef TRAVELING_SALESMAN_PRINT_H
+#define TRAVELING_SALESMAN_PRINT_H
+
+
+#include "../City.h"
+
+void Print(const std::vector<City>& cities);
+
+
+#endif //TRAVELING_SALESMAN_PRINT_H
