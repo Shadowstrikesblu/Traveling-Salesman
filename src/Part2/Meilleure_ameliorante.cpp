b54ba34fd1ec1389567ba8f28c82c15aec163a89
Shadowstrikesblu <hmoulk8@gmail.com>
2023-06-03 11:56:36 +0200

    Commmencé la partie 2

---
+++ src/Part2/Meilleure_ameliorante.cpp
@@ -0,0 +1,5 @@
+//
+// Created by hmoul on 03/06/2023.
+//
+
+#include "Meilleure_ameliorante.h"
---
+++ src/Part2/Swapping.h
@@ -0,0 +1,13 @@
+//
+// Created by hmoul on 02/06/2023.
+//
+
+#ifndef TRAVELING_SALESMAN_SWAPPING_H
+#define TRAVELING_SALESMAN_SWAPPING_H
+
+
+#include <vector>
+
+void Swapping(std::vector<int>& cities, int cityIndex1, int cityIndex2);
+
+#endif //TRAVELING_SALESMAN_SWAPPING_H
